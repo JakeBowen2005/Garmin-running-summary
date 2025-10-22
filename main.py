5290d5979af68487f1d169468f47442fe0251794
Jake Bowen <jakeeb05@gmail.com>
2025-10-21 20:37:56 -0700

    Initial commit - project setup

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+#Steps
+#Get and clean data / make sure all strings are numbers, missing values are filled and we added the columns we need to summarize
+
+
+
+file =input("Input path to garmin csv: ")
+
+
