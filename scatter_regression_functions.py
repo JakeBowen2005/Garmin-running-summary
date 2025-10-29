e12043fd7a37a469ecfc7c084da70a37bbe8e443
Jake Bowen <jakeeb05@gmail.com>
2025-10-28 19:50:29 -0700

    Fully added regression line with coloring each data point for the scatter with regards to distance and put a text box, want all graphs to be this professional

--- scatter_regression_functions.py
+++ scatter_regression_functions.py
@@ -4,21 +4,37 @@ import time_pace_func
 import numpy as np
 #Scatterplot to show a correlation between average pace and average heart rate
 def hr_pace_scatter(data):
-    plt.figure(figsize=(10,8))
-    plt.scatter(data["Avg Pace"], data["Avg HR"], color="royalblue", alpha=0.5)
-    plt.grid(True, linestyle="--", alpha=0.4)
-
-    #Adding linear regression line
+    #Fit the regression line
     z = np.polyfit(data["Avg Pace"], data["Avg HR"], 1)
     # np.polyfit(x,y,1) fits a polynomial of degree 1 return [m,b] in y=m(x)+b
     p = np.poly1d(z)
     #Converts that [m, b] array into a callable function.
     # You can now compute predicted y-values: p(150) gives pace at HR=150.
+
+    predicted_hr = p(data["Avg Pace"])
+    ss_res = np.sum((data["Avg HR"] - predicted_hr) ** 2)
+    ss_tot = np.sum((data["Avg HR"] - np.mean(data["Avg HR"])) ** 2)
+    r2 = 1-(ss_res/ss_tot)
+
+    plt.figure(figsize=(10,8))
+    sc = plt.scatter(data["Avg Pace"], data["Avg HR"], c=data["Distance"],cmap="viridis", alpha=0.5)
+    plt.grid(True, linestyle="--", alpha=0.4)
     plt.plot(data["Avg Pace"], p(data["Avg Pace"]), color="red", label="Trendline")
 
+    slope, intercept = z
 
-    plt.title("Avergae Pace vs Heart Rate")
+    plt.text(
+        x=data["Avg Pace"].min() + 0.1,
+        y=data["Avg HR"].max() -5,
+        s=f"y = {slope:.2f}x + {intercept:.1f}\nR² = {r2:.2f}",
+        fontsize=11, color="black", bbox=dict(facecolor="white", alpha=0.7)
+    )
+
+    #Titles
+    plt.colorbar(sc, label="Distance (miles)")  
+    plt.title("Heart Rate vs Pace with Distance Coloring and Regression Line")
     plt.xlabel("Average Pace (min/mile)")
     plt.ylabel("Average Heart Rate")
     plt.tight_layout()
+    plt.savefig("output/visuals/pace_hr_regression.png", dpi=300, bbox_inches="tight")
     plt.show()
